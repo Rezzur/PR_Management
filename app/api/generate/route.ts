90db809dfd10d6b7b73cd9b83d996c6be21ebe28
Yan <zyrianov.ian@gmail.com>
2026-04-09 23:10:09 +0300

    Change model to pro

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -40,9 +40,8 @@ export async function POST(req: Request) {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY ?? "");
   let resultString = "";
   try {
-    const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash" 
-    });
+    // Пробуем самую стабильную версию
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const generation = await model.generateContent({
       contents: [{ role: "user", parts: [{ text: prompt }] }],
     });
